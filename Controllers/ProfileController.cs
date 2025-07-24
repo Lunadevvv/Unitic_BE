b0a77e699e6741350a684552a598b235b8e061f6
Lunadev <vuminhdat98@gmail.com>
2025-07-25 00:48:30 +0700

    fix response profile

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -42,7 +42,7 @@ namespace Unitic_BE.Controllers
                 LastName = user.LastName,
                 Mssv = user.Mssv,
                 Wallet = user.wallet,
-                University = await _universityService.GetUniversityById(user.UniversityId)
+                University = await _universityService.GetUniversityById(user.UniversityId),
                 Role = user.Role
             });
         }
